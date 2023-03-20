115a4b2dd208d869d395425ef959c104e83f3431
juhui88 <sand8594@hufs.ac.kr>
2023-03-20 15:31:09 +0900

    이름순으로 수정

--- components/Bar.js
+++ components/Bar.js
@@ -2,13 +2,17 @@ import { useRouter } from "next/router"
 import data from "../pages/api/recruit.json"
 
 export default function Bar(){
+    const members = data.sort(function(a,b){
+        return b.name - a.name
+    })
+    console.log(members)
     const router = useRouter();
     const onClick = (id) => {
         router.push(`/new/${id}`)
     }
     
     return(<div className="grid grid-cols-10">
-        {data.map(i=>
+        {members.map(i=>
             <div key={i.id} onClick={()=>onClick(i.id)} className="hover:font-extrabold">{i.name}</div>
         )}
     </div>)
